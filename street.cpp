6f1cccc73d5291a9d7bc700a3f34ba78cd8c028f
Everymen <gajada89@gmail.com>
2020-05-09 14:49:51 -0400

    Add constructor to street.cpp

--- street.cpp
+++ street.cpp
@@ -1,6 +1,7 @@
 #include "street.h"
 
-Street::Street()
+Street::Street(QString street_name, int x1, int y1, int x2, int y2)
+    : street_name(street_name),
+    coordinates(QPair<QPoint, QPoint>(QPoint(x1, y1), QPoint(x2, y2)))
 {
-
 }
